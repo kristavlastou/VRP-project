e344a98f433c7e4c127dde6633f004b3bd7d18dc
Krista Vlastou <149063561+kristaa1@users.noreply.github.com>
2024-12-26 17:12:20 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,52 +1,74 @@
 import math
 
+
+class Model:
+
+    def __init__(self):
+        self.allNodes = []
+        self.customers = []
+        self.matrix = []
+        self.capacity = -1
+        
+
+    def BuildModel(self):
+        
+        with open('data.txt', 'r') as file:  
+         lines = file.readlines()
+         for line in lines[5:]:  
+            parts = line.strip().split(',')
+            ID = int(parts[0])
+            x = int(parts[1])
+            y = int(parts[2])
+            demand = float(parts[3])
+            self.allNodes.append(Node(ID, x, y, demand))
+         depot = self.allNodes[0]
+         self.capacity = 10
+         totalCustomers = len(self.allNodes) - 1
+         self.customers = self.allNodes[1:]
+        
+         self.matrix = self.calculate_distance_matrix()
+        
+        #print("All nodes:") #debug
+        #for node in self.allNodes:
+                #print(f"Node ID: {node.ID}, x: {node.x}, y: {node.y}, demand: {node.demand}")
+
+    def distance(self,from_node, to_node):
+        dx = from_node.x - to_node.x
+        dy = from_node.y - to_node.y
+        dist = math.sqrt(dx ** 2 + dy ** 2)
+        return dist
+    
+    def calculate_distance_matrix(self):
+        n = len(self.allNodes)  
+        distance_matrix = [[0] * n for _ in range(n)]  
+        
+       
+        for i in range(n):
+            for j in range(n):
+                if i != j:  
+                    distance_matrix[i][j] = self.distance(self.allNodes[i], self.allNodes[j])
+
+        return distance_matrix         
+    
+
 class Node:
     def __init__(self, ID, x, y, demand):
         self.ID = ID
         self.x = x
         self.y = y
         self.demand = demand
-
-    
-nodes = []
-
-
-with open('data.txt', 'r') as file:  
-    lines = file.readlines()
-    for line in lines[5:]:  
-        parts = line.strip().split(',')
-        ID = int(parts[0])
-        x = int(parts[1])
-        y = int(parts[2])
-        demand = float(parts[3])
-        nodes.append(Node(ID, x, y, demand))
-
-def distance(from_node, to_node):
-    dx = from_node.x - to_node.x
-    dy = from_node.y - to_node.y
-    dist = math.sqrt(dx ** 2 + dy ** 2)
-    return dist
-
-def calculate_distance_matrix(nodes):
-    n = len(nodes)
-    distance_matrix = [[0] * n for _ in range(n)]  
-    
-    for i in range(n):
-        for j in range(n):
-            if i != j:  
-                distance_matrix[i][j] = distance(nodes[i], nodes[j])
-    
-    return distance_matrix
-
-distance_matrix = calculate_distance_matrix(nodes)
+        self.isRouted = False
 
 
 class Route:
     def __init__(self, dp, cap):
         self.sequenceOfNodes = []
         self.sequenceOfNodes.append(dp)
-        self.sequenceOfNodes.append(dp)
+        #self.sequenceOfNodes.append(dp) μαλλον αχρειαστο γιατί θέλουμε ανοιχτές διαδρομές
         self.cost = 0
         self.capacity = cap
         self.load = 0
 
+
+model = Model()
+model.BuildModel() 
