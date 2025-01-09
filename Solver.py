d4f9b87aa9ce0e0c337870a344ddbfd7d420d7d0
Andriana Bilali <148336032+AndrianaBilali@users.noreply.github.com>
2025-01-09 15:49:32 +0200

    Update Solver.py

--- Solver.py
+++ Solver.py
@@ -99,6 +99,94 @@ class Solver:
         # self.penalized_n2_ID = -1
 
 
+    def Clarke_n_Wright(self):
+    self.sol = self.create_initial_routes()
+    savings: list = self.calculate_savings()
+    savings.sort(key=lambda s: s.score, reverse=True)
+    for i in range(0, len(savings)):
+        sav = savings[i]
+        n1 = sav.n1
+        n2 = sav.n2
+        rt1 = n1.route
+        rt2 = n2.route
+
+        if n1.route == n2.route:
+            continue
+        if self.not_first_or_last(rt1, n1) or self.not_first_or_last(rt2, n2):
+            continue
+        if rt1.load + rt2.load > self.capacity:
+            continue
+
+        self.merge_routes(n1, n2)
+
+        self.sol.cost -= sav.score
+
+
+    def calculate_savings(self):
+        savings = []
+        for i in range(0, len(self.customers)):
+            n1 = self.customers[i]
+            for j in range(i + 1, len(self.customers)):
+                n2 = self.customers[j]
+    
+                score = self.distance_matrix[n1.ID][self.depot.ID]
+                score += self.distance_matrix[self.depot.ID][n2.ID]
+                score -= self.distance_matrix[n1.ID][n2.ID]
+    
+                sav = Saving(n1, n2, score)
+                savings.append(sav)
+        return savings
+
+
+    def create_initial_routes(self):
+        s = Solution()
+        for i in range(0, len(self.customers)):
+            n = self.customers[i]
+            rt = Route(self.depot, self.capacity)
+            n.route = rt
+            n.position_in_route = 1
+            rt.sequenceOfNodes.insert(1, n)
+            rt.load = n.demand
+            rt.cost = self.calc_tonne_kilometre_route(rt)
+            s.routes.append(rt)
+            s.cost += rt.cost
+        return s
+
+
+    def not_first_or_last(self, rt, n):
+        if n.position_in_route != 1 and n.position_in_route != len(rt.sequenceOfNodes) - 2:
+            return True
+        return False
+
+
+    def merge_routes(self, n1, n2):
+        rt1 = n1.route
+        rt2 = n2.route
+    
+        if n1.position_in_route == 1 and n2.position_in_route == len(rt2.sequenceOfNodes) - 2:
+            rt1.sequenceOfNodes[1:1] = rt2.sequenceOfNodes[1:len(rt2.sequenceOfNodes) - 1]
+        elif n1.position_in_route == 1 and n2.position_in_route == 1:
+            rt1.sequenceOfNodes[1:1] = rt2.sequenceOfNodes[len(rt2.sequenceOfNodes) - 2:0:-1]
+        elif n1.position_in_route == len(rt1.sequenceOfNodes) - 2 and n2.position_in_route == 1:
+            rt1.sequenceOfNodes[len(rt1.sequenceOfNodes) - 1:len(rt1.sequenceOfNodes) - 1] = rt2.sequenceOfNodes[
+                                                                                             1:len(rt2.sequenceOfNodes) - 1]
+        elif n1.position_in_route == len(rt1.sequenceOfNodes) - 2 and n2.position_in_route == len(rt2.sequenceOfNodes) - 2:
+            rt1.sequenceOfNodes[len(rt1.sequenceOfNodes) - 1:len(rt1.sequenceOfNodes) - 1] = rt2.sequenceOfNodes[
+                                                                                             len(rt2.sequenceOfNodes) - 2:0:-1]
+        rt1.load += rt2.load
+        self.sol.routes.remove(rt2)
+        self.update_route_customers(rt1)
+
+
+    def update_route_customers(self, rt):
+        for i in range(1, len(rt.sequenceOfNodes) - 1):
+            n = rt.sequenceOfNodes[i]
+            n.route = rt
+            n.position_in_route = i
+
+
+
+    
     def calc_tonne_kilometre_route(self, rt):
         nodes_sequence = rt.sequenceOfNodes
         tot_dem = sum(n.demand for n in nodes_sequence)
